a061778118ffd3a71b3fc847f497d2fbf7670da9
shahinreza9702 <shahinr9701@hotmail.com>
2026-09-13 01:33:36 +0600

    Necessary icons images  data file added and technologyCard created with daisyui card

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import type { ITechnology } from "../types/technologyTypes";
+import TechnologyCard from "./TechnologyCard";
 
 interface TechnologyProps {
   technologiesPromise: Promise<ITechnology[]>;
@@ -7,8 +8,21 @@ interface TechnologyProps {
 
 const Technology = ({ technologiesPromise }: TechnologyProps) => {
   const technologies = use(technologiesPromise);
-  console.log(technologies, "Technologies");
-  return <div></div>;
+
+  return (
+    <section className="w-full base-100">
+      <div className="container mx-auto grid grid-cols-4">
+        {technologies.map((technology: ITechnology) => {
+          return (
+            <TechnologyCard
+              key={technology.id}
+              technology={technology}
+            ></TechnologyCard>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Technology;
